da0e69b26bb8f9258771dd9597190cfc6e66870b
Zayan Rashid Rana <zayan.rana901@gmail.com>
2024-01-24 20:11:12 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Zayan Rashid Rana",
+  title: "Machine Learning Expert",
+  email: "zayan.rana903@gmail.com",
+  gitHub: "https://github.com/ZayanRashid295",
+  instagram: "https://www.instagram.com/zayanrashid295",
+  linkedIn: "https://linkedin.com/in/zayan-rashid-73b937251/",  // Add "https://"
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
